89b25660358c6f35c20891d2b7fd4b187db07b3a
Florian Mondon <florian.mondon@clermont.in2p3.fr>
2017-08-28 11:59:33 +0200

    mB determination

--- read_jla.py
+++ read_jla.py
@@ -34,230 +34,217 @@ def is_number(s):
 # delta_mag = A(lambda) = [a(x)Rv+b(x)]E(B-V)
 
 def wave_eff_A(wave, trans, Rv=3.1):
-	sn_name = 'lc-SDSS6108.list'
-        EBV = read_lcSDSS(sn_name)['@MWEBV']
-	z = read_lcSDSS(sn_name)['@Z_HELIO']
-        rest_wave = wave*(1+np.array(z))
-        spl = Spline1d(rest_wave, trans, k=1, ext = 1)
-        dt = 10000
-        xs = np.linspace(min(rest_wave), max(rest_wave), dt)
-        dxs = ((max(rest_wave)-min(rest_wave))/dt)*np.ones(len(xs))
-        w_eff =  (np.sum((10**(-ccm89(xs,Rv*EBV,Rv)/2.5))*spl(xs)*xs*dxs)/np.sum((10**(-ccm89(xs,Rv*EBV,Rv)/2.5))*spl(xs)*dxs))/(1+z)
-	return w_eff
+    sn_name = 'lc-SDSS6108.list'
+    EBV = read_lcSDSS(sn_name)['@MWEBV']
+    z = read_lcSDSS(sn_name)['@Z_HELIO']
+    rest_wave = wave*(1+np.array(z))
+    spl = Spline1d(rest_wave, trans, k=1, ext = 1)
+    dt = 10000
+    xs = np.linspace(min(rest_wave), max(rest_wave), dt)
+    dxs = ((max(rest_wave)-min(rest_wave))/dt)*np.ones(len(xs))
+    w_eff =  (np.sum((10**(-ccm89(xs,Rv*EBV,Rv)/2.5))*spl(xs)*xs*dxs)/np.sum((10**(-ccm89(xs,Rv*EBV,Rv)/2.5))*spl(xs)*dxs))/(1+z)
+    return w_eff
 
 def A_l(xx, EBV, Rv=3.1):
-	A_l = []
-	xx = np.array(xx)
-	x = 1./(xx/10**4.) # xx in AA
-	for i in x:
-		if i < 1.1:
-			y = i**1.61
-			a = 0.574*y
-			b = -0.527*y
-		elif 1.1 <= i <= 3.3:
-			y = i - 1.82
-			a = 1 + y*(0.17699 + y*(-0.50447 + y*(-0.02427+ y*(0.72085 + y*(0.01979 + y*(-0.77530 + 0.32999*y))))))
-			b =  y*(1.41338 + y*(2.28305 + y*(1.07233 +y*(-5.38434 + y*(-0.62251 + y*(5.30260 - 2.09002*y))))))
-		elif 3.3 < i < 5.9:
-			a = 1.752 - 0.316*i - 0.104/((i-4.67)*(i-4.67) + 0.341)
-			b = -3.09 + 1.825*i + 1.206/((i-4.62)*(i-4.62) + 0.263)
-		elif 5.9 <= i <= 8.:
-			Fa = -(i-5.9)*(i-5.9) * (0.04473 + 0.009779*(i-5.9))
-			a = 1.752 - 0.316*i - 0.104/((i-4.67)*(i-4.67) + 0.341) + Fa
-			Fb = (i-5.9)*(i-5.9) * (0.213 + 0.1207*(i-5.9))
-			b = -3.09 + 1.825*i + 1.206/((i-4.62)*(i-4.62) + 0.263) + Fb
-		A = (a*Rv + b)*EBV
-		A_l.append(A)
-	A_l = np.array(A_l)
-	return A_l
+    A_l = []
+    xx = np.array(xx)
+    x = 1./(xx/10**4.) # xx in AA
+    for i in x:
+        if i < 1.1:
+            y = i**1.61
+            a = 0.574*y
+            b = -0.527*y
+        elif 1.1 <= i <= 3.3:
+            y = i - 1.82
+            a = 1 + y*(0.17699 + y*(-0.50447 + y*(-0.02427+ y*(0.72085 + y*(0.01979 + y*(-0.77530 + 0.32999*y))))))
+            b =  y*(1.41338 + y*(2.28305 + y*(1.07233 +y*(-5.38434 + y*(-0.62251 + y*(5.30260 - 2.09002*y))))))
+        elif 3.3 < i < 5.9:
+            a = 1.752 - 0.316*i - 0.104/((i-4.67)*(i-4.67) + 0.341)
+            b = -3.09 + 1.825*i + 1.206/((i-4.62)*(i-4.62) + 0.263)
+        elif 5.9 <= i <= 8.:
+            Fa = -(i-5.9)*(i-5.9) * (0.04473 + 0.009779*(i-5.9))
+            a = 1.752 - 0.316*i - 0.104/((i-4.67)*(i-4.67) + 0.341) + Fa
+            Fb = (i-5.9)*(i-5.9) * (0.213 + 0.1207*(i-5.9))
+            b = -3.09 + 1.825*i + 1.206/((i-4.62)*(i-4.62) + 0.263) + Fb
+        A = (a*Rv + b)*EBV
+        A_l.append(A)
+    A_l = np.array(A_l)
+    return A_l
 
 def wl_cut_salt2(fname,EBV,z):
-	filt = sncosmo.get_bandpass(fname)
-	wlen = filt.wave
+    filt = sncosmo.get_bandpass(fname)
+    wlen = filt.wave
+    tran = filt.trans
+    dt = 10000
+    spl = Spline1d(wlen, tran, k=1, ext = 1)
+    xs = np.linspace(min(wlen), max(wlen), dt)
+    dxs = ((max(wlen)-min(wlen))/(dt-1))
+    wlen_eff = np.sum((10**(-A_l(xs,EBV)/2.5))*spl(xs)*xs*dxs)/np.sum((10**(-A_l(xs,EBV)/2.5))*spl(xs)*dxs)
+    if wl_min_sal >= wlen_eff/(1+z) or wlen_eff/(1+z) >= wl_max_sal:
+        return ('False', wlen_eff/(1+z))
+    else:
+        return('True', wlen_eff/(1+z))
+
+def wl_cut_sugar(fname,z):        
+        filt = sncosmo.get_bandpass(fname)
+        wlen = filt.wave
         tran = filt.trans
-	dt = 10000
-        spl = Spline1d(wlen, tran, k=1, ext = 1)
+        dt = 10000
+        wlen_shift = wlen/(1+z)
+        spl = Spline1d(wlen_shift, tran, k=1, ext = 1)
+        #print
+        xs = np.linspace(min(wlen_shift), max(wlen_shift), dt)
+        dxs = ((max(wlen_shift)-min(wlen_shift))/(dt-1)) 
+        area_full = np.sum(spl(xs)*dxs) # full area under the filter
 
-        xs = np.linspace(min(wlen), max(wlen), dt)
-        dxs = ((max(wlen)-min(wlen))/(dt-1))
-	wlen_eff = np.sum((10**(-A_l(xs,EBV)/2.5))*spl(xs)*xs*dxs)/np.sum((10**(-A_l(xs,EBV)/2.5))*spl(xs)*dxs)
-	if wl_min_sal >= wlen_eff/(1+z) or wlen_eff/(1+z) >= wl_max_sal:
-		return ('False', wlen_eff/(1+z))
-	else:
-		return('True', wlen_eff/(1+z))
+        xs_cut = np.linspace(wl_min_sug, wl_max_sug, dt)
+        dxs_cut = ((wl_max_sug-wl_min_sug)/(dt-1))
+        area_cut = np.sum(spl(xs_cut)*dxs_cut)
+        r = 1.-area_cut/area_full # area under the filter outside the model
 
-def wl_cut_sugar(fname,z):		
-		filt = sncosmo.get_bandpass(fname)
-		wlen = filt.wave
-        	tran = filt.trans
-		dt = 10000
-		wlen_shift = wlen/(1+z)
-		spl = Spline1d(wlen_shift, tran, k=1, ext = 1)
-		#print
-		xs = np.linspace(min(wlen_shift), max(wlen_shift), dt)
-		dxs = ((max(wlen_shift)-min(wlen_shift))/(dt-1)) 
-		area_full = np.sum(spl(xs)*dxs) # full area under the filter
-
-		xs_cut = np.linspace(wl_min_sug, wl_max_sug, dt)
-		dxs_cut = ((wl_max_sug-wl_min_sug)/(dt-1))
-		area_cut = np.sum(spl(xs_cut)*dxs_cut)
-		r = 1.-area_cut/area_full # area under the filter outside the model
-
-		if r < 0.1:
-			return ('True',r)
-		else:
-			return ('False',r)
+        if r < 0.1:
+            return ('True',r)
+        else:
+            return ('False',r)
 
 
 def read_lc_jla(sn_name, model = None):
-	infile = open('jla_data/jla_light_curves/'+ sn_name, 'r') # download the sn data
-	numbers = []
-	d = []
-	words = []
-	words_new = []
-	head = {}
-	for line in infile:
-		if line[0] == '@': 
-			d = line.split()
-			if is_number(d[1]):
-				head[d[0]] = float(d[1])
-			else:
-				head[d[0]] = d[1]
-			continue
-		elif line[0] == '#':  # miss the lines started from #
-			continue	
-		elif len(line) == 1:  # miss empty lines
-			continue
-		words = line.split()	
-		words_new.append(float(words[0]))
-		words_new.append(float(words[1]))
-		words_new.append(float(words[2]))
-		words_new.append(float(words[3]))
-		words_new.append('jla_' + words[4])
-		words_new.append('jla_' + words[5])
-		numbers.append(words_new)
-		words_new = []
-	
-#	numbers.append(words)
-	infile.close()
+    infile = open('jla_data/jla_light_curves/'+ sn_name, 'r') # download the sn data
+    numbers = []
+    d = []
+    words = []
+    words_new = []
+    head = {}
+    for line in infile:
+        if line[0] == '@': 
+            d = line.split()
+            if is_number(d[1]):
+                head[d[0]] = float(d[1])
+            else:
+                head[d[0]] = d[1]
+            continue
+        elif line[0] == '#':  # miss the lines started from #
+            continue    
+        elif len(line) == 1:  # miss empty lines
+            continue
+        words = line.split()    
+        words_new.append(float(words[0]))
+        words_new.append(float(words[1]))
+        words_new.append(float(words[2]))
+        words_new.append(float(words[3]))
+        words_new.append('jla_' + words[4])
+        words_new.append('jla_' + words[5])
+        numbers.append(words_new)
+        words_new = []
+    
+#    numbers.append(words)
+    infile.close()
 
-	dic = {}
-	for x in numbers:
-		if x[4] in dic.keys():
-			dic[x[4]] += 1
-		else:
-			dic[x[4]] = 1
+    dic = {}
+    for x in numbers:
+        if x[4] in dic.keys():
+            dic[x[4]] += 1
+        else:
+            dic[x[4]] = 1
 
-	f_in = {}	
-	f_out = {}
-	for i in dic.keys():
-		if model == 'salt2':
-			res = wl_cut_salt2(i,head['@MWEBV'],head['@Z_HELIO']) 
-			if res[0] == 'True':
-				f_in[i] = res[1]
-			else:
-				f_out[i] = res[1]
-				print 'We excluded passband %s (%d points) because restframewavelength = %7.3f does not belong to the interval [%d,%d]' % (i,dic[i],res[1],wl_min_sal,wl_max_sal)
-		elif model == 'sugar':
-			res = wl_cut_sugar(i,head['@Z_HELIO'])
-			if res[0] == 'True':
-				f_in[i] = res[1]
-			else:
-				f_out[i] = res[1]
-				print 'We excluded passband %s (%d points) because it does not belong to the interval [%d,%d]' % (i,dic[i],wl_min_sug,wl_max_sug)
-		else:
-			print 'ERROR: model name has to be salt2 or sugar'
-	time = []
-	band = []
-	flux = []
-	fluxerr = []
-	zp = []
-	zpsys = []
-	for x in numbers:
-		if x[4] in f_in.keys():
-			time.append(x[0])
-			band.append(x[4])
-			flux.append(x[1])
-			fluxerr.append(x[2])
-			zp.append(x[3])
-			zpsys.append(x[5])
-		else:
-			continue
-	data=Table([time,band,flux,fluxerr,zp,zpsys], names=('time','band','flux','fluxerr','zp','zpsys'),meta={'name':'data'})
-        return head, data	
-		
+    f_in = {}    
+    f_out = {}
+    for i in dic.keys():
+        if model == 'salt2':
+            res = wl_cut_salt2(i,head['@MWEBV'],head['@Z_HELIO']) 
+            if res[0] == 'True':
+                f_in[i] = res[1]
+            else:
+                f_out[i] = res[1]
+                print 'We excluded passband %s (%d points) because restframewavelength = %7.3f does not belong to the interval [%d,%d]' % (i,dic[i],res[1],wl_min_sal,wl_max_sal)
+        elif model == 'sugar':
+            res = wl_cut_sugar(i,head['@Z_HELIO'])
+            if res[0] == 'True':
+                f_in[i] = res[1]
+            else:
+                f_out[i] = res[1]
+                print 'We excluded passband %s (%d points) because it does not belong to the interval [%d,%d]' % (i,dic[i],wl_min_sug,wl_max_sug)
+        else:
+            print 'ERROR: model name has to be salt2 or sugar'
+    time = []
+    band = []
+    flux = []
+    fluxerr = []
+    zp = []
+    zpsys = []
+    for x in numbers:
+        if x[4] in f_in.keys():
+            time.append(x[0])
+            band.append(x[4])
+            flux.append(x[1])
+            fluxerr.append(x[2])
+            zp.append(x[3])
+            zpsys.append(x[5])
+        else:
+            continue
+    data=Table([time,band,flux,fluxerr,zp,zpsys], names=('time','band','flux','fluxerr','zp','zpsys'),meta={'name':'data'})
+    return head, data    
+        
 def results(filename):
-	salt2_param = open('/home/maria/Dropbox/Science/Supernovae/'+filename, 'r')
-	lines = salt2_param.readlines()
-	salt2_param.close()
-	data = {}
-	first_line = lines[0]
-	properties = first_line.split()
+    salt2_param = open(filename, 'r')
+    lines = salt2_param.readlines()
+    salt2_param.close()
+    data = {}
+    first_line = lines[0]
+    properties = first_line.split()
 
-	for line in lines[1:]:
-	    words = line.split()
-	    i = words[0]
-	    values = words[1:]
-	    data[i] = {}
-	    for p, v in zip(properties[1:], values):
-	            data[i][p] = float(v)
+    for line in lines[1:]:
+        words = line.split()
+        i = words[0]
+        values = words[1:]
+        data[i] = {}
+        for p, v in zip(properties[1:], values):
+                data[i][p] = float(v)
 
-	return data
+    return data
 
-def comparison_plot(par='tmax',er_par = 'dtmax'):
-	x = 0
-	for key in results('sncosmo/res_com'):
-		#dif = results('sncosmo/res_SDSS.txt')[key][par]-results('JLA_SALT2/jla_lcparams.txt')[key][par]
-		#if abs(dif) > 2.3:
-		#if results('sncosmo/res_com')[key][er_par] > 1:
-		#	print key
-		#plt.errorbar(results('sncosmo/res_SDSS.txt')[key][par],results('JLA_SALT2/jla_lcparams.txt')[key][par],xerr=results('sncosmo/res_SDSS.txt')[key][er_par],yerr=results('JLA_SALT2/jla_lcparams.txt')[key][er_par],marker = 'o',color='red')
-		plt.errorbar(x,results('sncosmo/res_com')[key][par]-results('JLA_SALT2/jla_lcparams.txt')[key][par],results('JLA_SALT2/jla_lcparams.txt')[key][er_par],marker = 'o',color='red')
-		#plt.plot(results('sncosmo/ALLSDSSres.txt')[key]['color'],results('JLA_SALT2/jla_lcparams.txt')[key]['color'],'ro')
-		#plt.plot(x,results('sncosmo/ALLSDSSres.txt')[key][par]-results('JLA_SALT2/jla_lcparams.txt')[key][par],'ro')
-		x = x + 1
-	#plt.plot(range(x),np.zeros(x),'k')
-	#plt.plot([53540,54650],[53540,54650],'k')
-	#plt.xlim([53540,54650])
-	#plt.ylim([53540,54650])
-	#plt.xlabel('sncosmo')
-	#plt.ylabel('snfit')
-	plt.ylabel('$t_{snfit}-t_{sncosmo}$',fontsize=25)
-	plt.xlabel('N')
-	plt.savefig('all_tmax_n.png')
-	plt.show()
+def comparison_plot(par='x1',er_par = 'dx1'):
+    x = 0
+    for key in results('results/res_salt2.txt'):
+        dif = results('results/res_salt2.txt')[key][par]-results('/data/software/jla_likelihood_v6/data/jla_lcparams.txt')[key][par]
+        if abs(dif) > 0.1:
+#        if results('sncosmo/res_com')[key][er_par] > 1:
+            print key
+        plt.errorbar(results('results/res_salt2.txt')[key][par],results('/data/software/jla_likelihood_v6/data/jla_lcparams.txt')[key][par],xerr=results('results/res_salt2.txt')[key][er_par],yerr=results('/data/software/jla_likelihood_v6/data/jla_lcparams.txt')[key][er_par],marker = 'o',color='red')
+#        plt.errorbar(x,results('results/res_salt2.txt')[key][par]-results('/data/software/jla_likelihood_v6/data/jla_lcparams.txt')[key][par],results('/data/software/jla_likelihood_v6/data/jla_lcparams.txt')[key][er_par],marker = 'o',color='red')
+#        plt.plot(results('results/res_salt2.txt')[key]['color'],results('/data/software/jla_likelihood_v6/data/jla_lcparams.txt')[key]['color'],'ro')
+        #plt.plot(x,results('sncosmo/ALLSDSSres.txt')[key][par]-results('/data/software/jla_likelihood_v6/data/jla_lcparams.txt')[key][par],'ro')
+        x = x + 1
+#    plt.plot(range(x),range(x),'k')
+    plt.plot([-4,4],[-4,4],ls='--',color='black')
+    plt.xlim([-4,4])
+    plt.ylim([-4,4])
+    plt.xlabel('sncosmo')
+    plt.ylabel('snfit')
+#    plt.ylabel('$t_{snfit}-t_{sncosmo}$',fontsize=25)
+#    plt.xlabel('N')
+#    plt.savefig('all_tmax_n.png')
+    plt.show()
 
 def comparison_hist(par='tmax',er_par = 'dtmax'):
 
-	gs = gridspec.GridSpec(2, 1) #subplots ratio
-	f, (ax1, ax2) = plt.subplots(2, sharex=True)
+    gs = gridspec.GridSpec(2, 1) #subplots ratio
+    f, (ax1, ax2) = plt.subplots(2, sharex=True)
 
-	ax1 = plt.subplot(gs[0, 0])
-	ax2 = plt.subplot(gs[1, 0])
+    ax1 = plt.subplot(gs[0, 0])
+    ax2 = plt.subplot(gs[1, 0])
 
-	dif_par = []
-	dif_er_par = []	
-	for key in results('sncosmo/NBdata'):
-		dif_par.append(results('sncosmo/NBdata')[key][par]-results('JLA_SALT2/jla_lcparams.txt')[key][par])
-		dif_er_par.append(results('sncosmo/NBdata')[key][er_par]-results('JLA_SALT2/jla_lcparams.txt')[key][er_par])
-	ax1.hist(dif_par,25,label=par)
-	ax2.hist(dif_er_par,25,label=er_par)
-	ax1.legend()
-	ax2.legend()
-	ax1.set_ylabel('N')
-	ax2.set_ylabel('N')
-	plt.savefig('nb_tmax.png')	
-	plt.show()
-
-def new_Table():
-    """
-    creat an empty astropy Table
-    """
-    time=[]
-    band=[]
-    flux=[]
-    fluxerr=[]
-    zp=[]
-    zpsys=[]    
-    return Table([time,band,flux,fluxerr,zp,zpsys], names=('time','band','flux','fluxerr','zp','zpsys'),meta={'name':'data'})
+    dif_par = []
+    dif_er_par = []    
+    for key in results('sncosmo/NBdata'):
+        dif_par.append(results('sncosmo/NBdata')[key][par]-results('JLA_SALT2/jla_lcparams.txt')[key][par])
+        dif_er_par.append(results('sncosmo/NBdata')[key][er_par]-results('JLA_SALT2/jla_lcparams.txt')[key][er_par])
+    ax1.hist(dif_par,25,label=par)
+    ax2.hist(dif_er_par,25,label=er_par)
+    ax1.legend()
+    ax2.legend()
+    ax1.set_ylabel('N')
+    ax2.set_ylabel('N')
+    plt.savefig('nb_tmax.png')    
+    plt.show()
 
--- sncosmo_jla.py
+++ sncosmo_jla.py
@@ -1,26 +1,30 @@
 #! /usr/bin/env python
 import sncosmo
-import builtins_jla
 import os
 from matplotlib import pyplot as plt
 from read_jla import read_lc_jla
-from read_jla import new_Table
 import numpy as np
 import copy
+from scipy.interpolate import InterpolatedUnivariateSpline as Spline1d
+import astropy.units as u
 
+clight=299792.458
+h=6.626070040*10**-34
 t_min = -15
 t_max = 45
 
 t_min_sug = -12
 t_max_sug = 42
 
-
+# wavelength limits for salt2 model
+wl_min_sal = 3000
+wl_max_sal = 7000
     
 def fit_salt2():
-    outfile = open('res_salt2.txt', 'w')
-    outfile.write('#name zcmb zhel dz mb dmb x1 dx1 color dcolor 3rdvar d3rdvar tmax dtmax cov_m_s cov_m_c cov_s_c set ra dec biascor')
-    list_SDSS = ['lc-SDSS17240.list','lc-SDSS12977.list']
-    #list_SDSS = [raw_input('Name of SN: ')]
+#    outfile = open('results/res_salt2.txt', 'w')
+#    outfile.write('#name zcmb zhel dz mb dmb x1 dx1 color dcolor 3rdvar d3rdvar tmax dtmax cov_m_s cov_m_c cov_s_c set ra dec biascor mb_sncosmo x0 dx0')
+    list_SDSS = ['lc-SDSS3377.list']
+#    list_SDSS = [raw_input('Name of SN: ')]
     fitfail=[]        
 #    for filename in os.listdir('./jla_data/jla_light_curves/'):
     for filename in list_SDSS:
@@ -46,7 +50,6 @@ def fit_salt2():
                 #initial iteration x1 fix
                 model.set(x1=0.0)
                 res, fitted_model = sncosmo.fit_lc(data, model, ['t0','x0', 'c'], modelcov = False, guess_t0=True)
-                chi2_res={}
                 chi2=res.chisq
                 chi2p=chi2+1
                 m=0
@@ -96,24 +99,34 @@ def fit_salt2():
                 #print fitted_model.maxtime(), fitted_model.mintime()
 #                print res
                 print 'number of iteration ',m
-                sncosmo.plot_lc(data, model=fitted_model, errors=res.errors)
+#                sncosmo.plot_lc(data, model=fitted_model, errors=res.errors)
 #                plt.savefig('SDSS9032_sugar.eps')
                 plt.show()
+#                mb=mB_determination(res)
+#                print mb
+                mb_sncosmo=fitted_model.bandmag('jla_STANDARD::B','jla_VEGA2', [res.parameters[1]])[0]
+                print fitted_model.bandmag('STANDARD::B','VEGA2', [res.parameters[1]])
             except:
                 fitfail.append(sn_name)
                 print 'Error: fit fail for ',sn_name
-
-
+            
+            
+            print(res.parameters)
             print fitfail
+
+#    return res
 #            outfile.write('%s %s \n' %(sn_name,fitted_model.bandmag('standard::b','jla1',fitted_model.parameters[1]))) 
 #            outfile.write('%s \n' %(res)) 
         
 
-            outfile.write('\n')
+#            outfile.write('\n')
         
-            outfile.write('%s 999 %f 999 %f %f %f %f %f %f 999 999 %f %f 999 999 999 999 999 999 999' %(sn_name[3:-5],res.parameters[0],res.parameters[2],res.errors['x0'],res.parameters[3],res.errors['x1'],res.parameters[4],res.errors['c'],res.parameters[1],res.errors['t0'])) 
+#            outfile.write('%s 999 %f 999 %f %f %f %f %f %f 999 999 %f %f 999 999 999 999 999 999 999' %(sn_name[3:-5],res.parameters[0],res.parameters[2],res.errors['x0'],res.parameters[3],res.errors['x1'],res.parameters[4],res.errors['c'],res.parameters[1],res.errors['t0'])) 
+#            outfile.write('%s 999 %f 999 %f %f %f %f %f %f 999 999 %f %f 999 999 999 999 999 999 999' %(sn_name[3:-5],res.parameters[0],mb,0,res.parameters[3],res.errors['x1'],res.parameters[4],res.errors['c'],res.parameters[1],res.errors['t0'])) 
             #outfile.write('%s 999 %f 999 %f %f %f %f %f %f 999 999 %f %f 999 999 999 999 999 999 999' %(sn_name[3:-5],res.parameters[0],res.parameters[2],res.errors['x0'],res.parameters[3],res.errors['x1'],res.parameters[4],res.errors['c'],t_peak,er_t_peak))  
-    outfile.close()
+#            outfile.write('%s 999 %f 999 %f %f %f %f %f %f 999 999 %f %f 999 999 999 999 999 999 999 %f %f %f' %(sn_name[3:-5],res.parameters[0],mb,0,res.parameters[3],res.errors['x1'],res.parameters[4],res.errors['c'],res.parameters[1],res.errors['t0'],mb_sncosmo,res.parameters[2],res.errors['x0'])) 
+            
+#    outfile.close()
 
 def fit1():
     #par_c = {}
@@ -247,8 +260,7 @@ def fit_sugar():
         
     #outfile.close()    
 
-def chi2(name=None):
-    sn_name='lc-SDSS17240.list'
+def chi2(sn_name):
     head, data = read_lc_jla(sn_name, model='salt2')
 
     source = sncosmo.get_source('salt2', version='2.4')
@@ -256,14 +268,145 @@ def chi2(name=None):
     source.z_snfit = head['@Z_HELIO']
     source.Rv_snfit = 3.1
     dust = sncosmo.CCM89Dust()
-
+    
+    #load snfit parameters 
+    snfit=np.loadtxt('./x0_mb_JLA.txt',dtype='str')
+    name_snfit=np.array(snfit[:,0])
+    mb_snfit=np.array(snfit[:,3],float)
+    dmb_snfit=np.array(snfit[:,4],float)
+    x0_snfit=np.array(snfit[:,5],float)
+    x1_snfit=np.array(snfit[:,7],float)
+    c_snfit=np.array(snfit[:,9],float)
+    t0_snfit=np.array(snfit[:,11],float)
+    z_snfit=np.array(snfit[:,13],float)
+    for i in range(len(name_snfit)):
+        sn_name_snfit='lc-'+name_snfit[i]+'.list'
+        if sn_name_snfit==sn_name:
+            x0_snfit=x0_snfit[i]
+            x1_snfit=x1_snfit[i]
+            c_snfit=c_snfit[i]
+            t0_snfit=t0_snfit[i]
+            mb_snfit=mb_snfit[i]
+            dmb_snfit=dmb_snfit[i]
+            z_snfit=z_snfit[i]
+            
     model = sncosmo.Model(source=source,effects=[dust],effect_names=['mw'],effect_frames=['obs'])
-    model.set(mwebv=head['@MWEBV'], z=head['@Z_HELIO'], t0=54337.325699, x0=0.000968961137087,x1=-1.466919,c=-0.121433) #
+    model.set(mwebv=head['@MWEBV'], z=head['@Z_HELIO'], t0=t0_snfit, x0=x0_snfit ,x1=x1_snfit,c=c_snfit) #
 
     print model.parameters
     print sncosmo.chisq(data,model,modelcov=True)
+    return model, mb_snfit,dmb_snfit,z_snfit
 #    print model.bandflux('jla_SDSS::g', [54277.5, 54285.0, 54292.5, 54300., 54307.5, 54315., 54322.5], zp=25, zpsys='jla_AB_B12')
     #for i in range(13):
     #    print model.bandflux(data[i][1], float(data[i][0]), zp=float(data[i][4]), zpsys='AB_jla')
-    sncosmo.plot_lc(data, model=model)
-    plt.show()
+#    sncosmo.plot_lc(data, model=model)
+#    plt.show()
+
+def mB_determination(res):
+    
+    
+    #interpolation of TB and Trest
+    filt2=np.genfromtxt('./jla_data/Instruments/SNLS3-Landolt-model/sb-shifted.dat')
+    wlen=filt2[:,0]
+    tran=filt2[:,1]
+    splB = Spline1d(wlen, tran, k=1,ext = 1)
+
+
+
+    #interpolation of ref spectrum
+    data = np.genfromtxt('./jla_data/MagSys/bd_17d4708_stisnic_003.ascii')
+    dispersion = data[:,0]
+    flux_density = data[:,1]
+    splref = Spline1d(dispersion, flux_density, k=1,ext = 1)
+
+  
+    #interpolation of the spectrum model
+    template_0=np.genfromtxt('./salt2-4/salt2_template_0.dat')    
+    template_1=np.genfromtxt('./salt2-4/salt2_template_1.dat')
+#    salt2source=sncosmo.SALT2Source('/users/divers/lsst/mondon/hubblefit/sncosmo_jla/salt2-4')
+    
+    wlM0=[]
+    M0=[]
+    for i in range(len(template_0[:,0])):
+        if template_0[:,0][i]==0.0:
+           wlM0.append(template_0[:,1][i]) 
+           M0.append(template_0[:,2][i])
+    splM0 = Spline1d(wlM0, M0, k=1,ext = 1)
+
+    wlM1=[]
+    M1=[]
+    for i in range(len(template_1[:,0])):
+        if template_1[:,0][i]==0.0:
+           wlM1.append(template_1[:,1][i]) 
+           M1.append(template_1[:,2][i])
+    splM1 = Spline1d(wlM1, M1, k=1,ext = 1)
+
+    #computation of the integral
+    dt = 100000
+    xs = np.linspace(float(wl_min_sal), float(wl_max_sal), dt)
+    dxs = (float(wl_max_sal-wl_min_sal)/(dt-1))
+#    cl=[]
+#    for i in xs:
+#        cl.append(color_law_salt2(xs[i]))
+#    I1=np.sum((splM0(xs)*10**-12+res.parameters[3]*splM1(xs)*10**-12)*(10**(-0.4*cl*res.parameters[4]))*xs*splB(xs)*dxs)
+
+    I1=np.sum((splM0(xs)*10**-12+res.parameters[3]*splM1(xs)*10**-12)*(10**(-0.4*color_law_salt2(xs)*res.parameters[4]))*xs*splB(xs)*dxs)    
+    I2=np.sum(splref(xs)*xs*splB(xs)*dxs)
+    print I1, I2   
+    
+    
+    
+    #computation of mb
+    mref=9.907
+    mb=-2.5*np.log10(res.parameters[2]*(I1/I2))+mref
+
+    return mb
+
+def color_law_salt2(wl):
+        B_wl=4302.57
+        V_wl=5428.55
+        l=(wl-B_wl)/(V_wl-B_wl)
+        l_lo=(2800.-B_wl)/(V_wl-B_wl)
+        l_hi=(7000.-B_wl)/(V_wl-B_wl)
+        a=-0.504294
+        b=0.787691
+        c=-0.461715
+        d=0.0815619
+        cst= 1-(a+b+c+d)
+        cl=[]
+        for i in range (len(l)):
+            if l[i] > l_hi:
+                cl.append(-(cst*l_hi+l_hi**2*a+l_hi**3*b+l_hi**4*c+l_hi**5*d+(cst+2*l_hi*a+3*l_hi**2*b+4*l_hi**3*c+5*l_hi**4*d)*(l[i]-l_hi)))
+            if l[i] < l_lo:
+                cl.append(-(cst*l_lo+l_lo**2*a+l_lo**3*b+l_lo**4*c+l_lo**5*d+(cst+2*l_lo*a+3*l_lo**2*b+4*l_lo**3*c+5*l_lo**4*d)*(l[i]-l_lo))) 
+            if l[i]>= l_lo and l[i]<= l_hi:
+                cl.append(-(cst*l[i]+l[i]**2*a+l[i]**3*b+l[i]**4*c+l[i]**5*d)) 
+        return np.array(cl)
+#        return -(cst*l+l**2*a+l**3*b+l**4*c+l**5*d)
+def Plot_diff_mb():
+    
+    mb_sncosmo=np.array(fit_salt2())
+    mb_snfittxt=np.loadtxt('/users/divers/lsst/mondon/hubblefit/sncosmo_jla/x0_mb_JLA.txt',dtype='str')
+    mb_snfit= np.array(mb_snfittxt[:,3],float)
+    dmb_snfit=np.array(mb_snfittxt[:,4],float)
+    plt.errorbar(mb_sncosmo,mb_snfit,xerr=np.nan,yerr=dmb_snfit,marker = 'o',color='red')
+    plt.xlabel('sncosmo')
+    plt.ylabel('snfit')
+    plt.show()    
+    
+def mb_using_snfit_parameters():
+    diff=[]
+    z=[]
+    for filename in os.listdir('./jla_data/jla_light_curves/'):
+#    for filename in list_SDSS:
+        if 'lc-SDSS' == filename[:7]:
+            sn_name = filename
+            res,mb_snfit1,dmb_snfit1,z1 =chi2(sn_name)
+            mb_snfit_code=mB_determination(res)
+            diff.append(mb_snfit_code-mb_snfit1)
+            z.append(z1)
+    plt.scatter(z,diff,marker = '.',color='red',label='mb')
+
+            
+            
+          
